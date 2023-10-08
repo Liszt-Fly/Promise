dff366920e4949e13426523b670894b2e2bf11c7
Liszt-Fly <1904231450@qq.com>
2023-10-08 21:03:45 +0800

    执行过程出错调用reject

--- mp.spec.ts
+++ mp.spec.ts
@@ -9,4 +9,20 @@ describe("MPromise", () => {
     });
     expect(m.status).to.equal(State.FULFILLED);
   });
+  it("resolve,reject不能同时使用", () => {
+    let m = new MPromise((resolve, reject) => {
+      resolve("hello,world");
+      reject("reject");
+    });
+    expect(m.status).toBe(State.FULFILLED);
+  });
+  it("如果在函数执行过程中出错,会直接调用reject", () => {
+    let m = new MPromise((resolve, reject) => {
+      //@ts-ignore
+      console.log(asd);
+      resolve("hello");
+    });
+
+    expect(m.status).toBe(State.REJECTED);
+  });
 });
